b883b2f6e1a2b9e10f9300bf299b0e6913dd5a2c
Diego T. Fialho <diego.tfialho@gmail.com>
2017-04-25 16:20:13 -0300

    Lifecycle hooks

--- ServersApplication/src/app/app.component.ts
+++ ServersApplication/src/app/app.component.ts
@@ -23,4 +23,12 @@ export class AppComponent {
       content: bluePrintData.serverContent
     });
   }
+
+  onChangeFirst() {
+    this.serverElements[0].name = "changed value";
+  }
+
+  onDestroyFirst() {
+    this.serverElements.splice(0, 1);
+  }
 }
