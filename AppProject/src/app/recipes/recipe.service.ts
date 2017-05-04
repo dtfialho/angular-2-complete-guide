1ddea2ed6df8fbb8121395ed0bff7f8864fb8f1b
Diego T. Fialho <diego.tfialho@gmail.com>
2017-05-04 10:59:17 -0300

    Managing recipes in a recipe service

---
+++ AppProject/src/app/recipes/recipe.service.ts
@@ -0,0 +1,12 @@
+import { Recipe } from './recipes.model';
+
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
