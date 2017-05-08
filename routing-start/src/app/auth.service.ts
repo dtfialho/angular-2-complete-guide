2df8f84df781a38c0c533d95aea4324ef74b47b1
Diego T. Fialho <diego.tfialho@gmail.com>
2017-05-08 17:05:25 -0300

    Protecting routes with canActivate

---
+++ routing-start/src/app/auth.service.ts
@@ -0,0 +1,22 @@
+export class AuthService {
+  loggedIn = false;
+
+  isAuthenticated() {
+    const promisse = new Promise(
+      (resolve, reject) => {
+        setTimeout(() => {
+          resolve(this.loggedIn);
+        }, 800);
+      }
+    );
+    return promisse;
+  }
+
+  login() {
+    this.loggedIn = true;
+  }
+
+  logout() {
+    this.loggedIn = false;
+  }
+}
