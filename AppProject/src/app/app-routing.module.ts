c2540770c5a8b9c2a407117064bd5c5f1dbb6e05
Diego T. Fialho <diego.tfialho@gmail.com>
2017-05-09 16:04:38 -0300

    Adding child routing together

--- AppProject/src/app/app-routing.module.ts
+++ AppProject/src/app/app-routing.module.ts
@@ -10,10 +10,14 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', component: RecipesComponent },
+  { path: 'recipes', component: RecipesComponent, children: [
+    { path: '', component: RecipeStartComponent },
+    { path: ':id', component: RecipeDetailComponent }
+  ] },
   { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
