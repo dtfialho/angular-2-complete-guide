5a8b5611ca1d19c56a053ac0e930d92a0f324035
Diego T. Fialho <diego.tfialho@gmail.com>
2017-05-16 16:56:10 -0300

    Understanding observable operators

--- observables-start/src/app/home/home.component.ts
+++ observables-start/src/app/home/home.component.ts
@@ -16,7 +16,7 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit() {
-    const myNumbers = Observable.interval(1000);
+    const myNumbers = Observable.interval(1000).map((data: number) => { return data * 2; });
     this.numberObsSubscription = myNumbers.subscribe(
       (number: number) => {
         console.log(number);
