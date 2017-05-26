c36ec66b7dcbb5cabed1d7def80160de357b9d2c
Diego T. Fialho <diego.tfialho@gmail.com>
2017-05-26 16:06:42 -0300

    Getting back the recipes

--- AppProject/src/app/header/header.component.ts
+++ AppProject/src/app/header/header.component.ts
@@ -27,4 +27,8 @@ export class HeaderComponent implements OnInit {
     );
   }
 
+  onGetData() {
+    this.dataStorageService.getRecipes();
+  }
+
 }
--- AppProject/src/app/shared/data-storage.service.ts
+++ AppProject/src/app/shared/data-storage.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipes.model';
 
 
 @Injectable()
@@ -12,5 +13,14 @@ export class DataStorageService {
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
     return this.http.put('https://ng-recipe-book-5b901.firebaseio.com/recipes.json', recipes);
+  }
+
+  getRecipes() {
+    this.http.get('https://ng-recipe-book-5b901.firebaseio.com/recipes.json').subscribe(
+      (response: Response) => {
+        const recipes: Recipe[] = response.json();
+        this.recipeService.setRecipes(recipes);
+      }
+    );
   }
 }
