493ebeefed79ff12320d142e8a04fbb911a4cc0e
Diego T. Fialho <diego.tfialho@gmail.com>
2017-05-26 10:26:35 -0300

    Sending requests (example: post request)

---
+++ http-start/src/app/server.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Injectable()
+export class ServerService {
+
+  constructor(private http: Http) { }
+
+  storeServers(servers: any[]) {
+    return this.http.post('https://udemy-ng-http-5eb32.firebaseio.com/data.json', servers);
+  }
+
+}
