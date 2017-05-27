0c1b81922a2f88677d1c0272293aa29afbd99e99
Diego T. Fialho <diego.tfialho@gmail.com>
2017-05-27 11:14:18 -0300

    Setting up the firebase SDK

--- AppProject/src/app/app.component.ts
+++ AppProject/src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+
+import { FirebaseConfig } from './firebase.config';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: FirebaseConfig.apiKey,
+      authDomain: FirebaseConfig.authDomain
+    });
+  }
 }
