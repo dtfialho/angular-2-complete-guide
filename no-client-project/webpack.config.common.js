3586046634f60a3201b0a63c4e9661070e851f47
Diego T. Fialho <diego.tfialho@gmail.com>
2017-06-09 10:22:48 -0300

    Finishing and using the development workflow

--- no-client-project/webpack.config.common.js
+++ no-client-project/webpack.config.common.js
@@ -1,26 +1,26 @@
-var htmlWebpackPlugin = require('html-webpack-plugin');;
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: './src/app/main.ts',
-	resolve: {
-		extensions: [ '.js', '.ts' ]
-	},
-	module: {
-		rules: [
-			{
-				test: /\.html$/,
-				loaders: [ 'html-loader' ]
-			},
-			{
-				test: /\.css$/,
-				loaders: [ 'raw-loader' ]
-			}
-		],
-		exprContextCritical: false
-	},
-	plugins: [
-		new htmlWebpackPlugin({
-			template: 'src/index.html'
-		})
-	]
+    entry: './src/app/main.ts',
+    resolve: {
+        extensions: ['.js', '.ts']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loaders: ['html-loader']
+            },
+            {
+                test: /\.css$/,
+                loaders: ['raw-loader']
+            }
+        ],
+        exprContextCritical: false
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ]
 };
